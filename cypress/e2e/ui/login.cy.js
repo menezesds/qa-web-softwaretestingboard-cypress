51425af5c63be9d470448ced240f93f9b044aefc
menezesds <menezesds@hotmail.com>
2024-02-26 20:54:38 -0300

    feat: add login scenarios and commands file

---
+++ cypress/e2e/ui/login.cy.js
@@ -0,0 +1,28 @@
+/// <reference types="Cypress"/>
+describe('UI Functional Test: Login', () => {
+    it('Should redirect to home page after successul login with correct username and password', () => {
+        cy.functional_login('test0001@cenglandb.com', '*t4K#^g*TT929Yq')
+        cy.wait(4000)
+        cy.get(':nth-child(2) > .greet > .logged-in').should('contain', 'Welcome, Test0001 Test!')
+    })
+    it('Should return failure when logging in with invalid username and password', () => {
+        cy.functional_login('invalid_user@test.com', 'invalid_password')
+        cy.get('.message-error > div').should('contain', 'The account sign-in was incorrect or your account is disabled temporarily. Please wait and try again later.')
+        
+    });
+    it('Should return failure when logging in with invalid password', () => {
+        cy.functional_login('test0001@cenglandb.com', 'invalid_password')
+        cy.get('.message-error > div').should('contain', 'The account sign-in was incorrect or your account is disabled temporarily. Please wait and try again later.')
+    });
+    it('Should return failure when logging in with empty password', () => {
+        cy.functional_login('test0001@cenglandb.com', '')
+        cy.wait(4000)
+        cy.get('.message-error > div').should('contain', 'A login and a password are required.')
+    });
+
+    it('Should return failure when logging in with empty username', () => {
+        cy.functional_login('', '*t4K#^g*TT929Yq')
+        cy.wait(4000)
+        cy.get('.message-error > div').should('contain', 'A login and a password are required.')
+    });
+});
---
+++ cypress/support/commands_login.js
@@ -0,0 +1,9 @@
+/// <reference types="Cypress"/>
+Cypress.Commands.add('functional_login', (username, password) => {
+    cy.visit('https://magento.softwaretestingboard.com/')
+    cy.get('.panel > .header > .authorization-link > a').contains('Sign In').click()
+    cy.get('.base').should('be.visible').should('contain', 'Customer Login')
+    if(username != '') cy.get('#email').type(username)
+    if(password != '') cy.get('#pass').type(password)
+    cy.get('div.primary > #send2 > span').should("contain", "Sign In").click()
+})
